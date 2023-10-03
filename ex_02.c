09cd28ae53a599c675f9488c6886bbfcebaaa7e4
Aluno <aluno@BL3-INFO01-19.maracanau.ifce.edu.br>
2023-10-03 14:49:37 -0300

    questao 2 feita

---
+++ ex_02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAMPALAVRA 20
+
+int main () {
+    char palavra[TAMPALAVRA];
+    char caractere;
+
+    puts("Insira uma palavra : ");
+    fgets(palavra , TAMPALAVRA , stdin);
+
+    puts("Insira um caractere : ");
+    scanf("%c" , &caractere);
+
+    for(int i = 0 ; i < (sizeof(palavra) / sizeof(palavra[0])) ; i++) {
+        if ( caractere == palavra[i] ) {
+            printf("Esse caractere %c encontrasse na palavra %s\n" , caractere ,  palavra);
+        }
+    }
+    
+    printf("O caractere não está na palavra %s" , palavra);
+}
