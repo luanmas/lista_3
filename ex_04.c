3d3f8db9cf8dd251201b07081fdd08eb1a8a019b
luanmas <luanmas27@gmail.com>
2023-10-07 19:01:21 -0300

    ex_04 feito

--- ex_04.c
+++ ex_04.c
@@ -4,23 +4,22 @@
 
 #define TAMPALAVRA 20
 
-int main () {
+int main() {
     char palavra[TAMPALAVRA];
     char palavra2[TAMPALAVRA];
-    int contador = 0;
 
     puts("Insira uma palavra : ");
-    fgets(palavra , TAMPALAVRA , stdin);
+    fgets(palavra, TAMPALAVRA, stdin);
 
-    puts("Insira outro palavra : ");
-    fgets(palavra2 , TAMPALAVRA , stdin);
+    puts("Insira outra palavra : ");
+    fgets(palavra2, TAMPALAVRA, stdin);
 
-
-    for(int i = 0 ; i < TAMPALAVRA ; i++) {
-        for (int j = 0; j < TAMPALAVRA ; i++) {
-            if(palavra[i] == palavra2[j]) {
-                contador++;
-            }
+    for (int i = 0; i <= strlen(palavra); i++) {
+        if (palavra[i] != palavra2[i]) {
+            puts("As palavras são diferentes");
+            exit(1);
         }
     }
+
+    puts("As palavras são iguais");
 }
